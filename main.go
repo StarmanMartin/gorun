d819cd34cc0f7852d84b8ff9075383239be6e17a
Maritn Starman <m.starman@live.com>
2016-01-27 17:22:43 +0100

    bug fix

--- main.go
+++ main.go
@@ -22,10 +22,10 @@ const (
 var runTypes = []string{install, test}
 
 var (
- lastPart *regexp.Regexp
- isTest, isBenchTest, isExecute, isWatch bool
- newRoot, packageName, currentPath, outputString string  
- restArgs []string
+	lastPart                                        *regexp.Regexp
+	isTest, isBenchTest, isExecute, isWatch         bool
+	newRoot, packageName, currentPath, outputString string
+	restArgs                                        []string
 )
 
 func init() {
@@ -38,13 +38,13 @@ func init() {
 	flag.StringVar(&outputString, "p", "", "Make Package")
 }
 
-func getCmd(cmdCommand []string) (*exec.Cmd) {
+func getCmd(cmdCommand []string) *exec.Cmd {
 	parts := cmdCommand
 	head := parts[0]
 	parts = parts[1:len(parts)]
 
 	cmd := exec.Command(head, parts...)
-   
+
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
@@ -53,9 +53,9 @@ func getCmd(cmdCommand []string) (*exec.Cmd) {
 
 func exeCmd(cmdCommand []string) (*exec.Cmd, error) {
 	cmd := getCmd(cmdCommand)
-    
+
 	if err := cmd.Start(); err != nil {
-		return cmd, err;
+		return cmd, err
 	}
 
 	return cmd, nil
@@ -126,24 +126,26 @@ func copyPackage(dir, packageName, funcName string) (isPackage bool, err error) 
 
 func main() {
 	flag.Parse()
-    var err error
+	var err error
 	newRoot, packageName, restArgs, err = handelPathArgs()
 	if err != nil {
 		log.Fatal(err)
 		return
 	}
-    
-    runBuild()
+	currentPath := os.Getenv("GOPATH")
+	defer func() {
+		log.Println("Done!!")
+		os.Setenv("GOPATH", currentPath)
+	}()
+	newPath := []string{newRoot, ";", currentPath}
+
+	os.Setenv("GOPATH", strings.Join(newPath, ""))
+	runBuild()
 }
 
 func runBuild() {
 	buildCommandList := buildCommand(packageName)
 	buildCommandList = append(buildCommandList, packageName)
-	currentPath := os.Getenv("GOPATH")
-	defer os.Setenv("GOPATH", currentPath)
-	newPath := []string{newRoot, ";", currentPath}
-
-	os.Setenv("GOPATH", strings.Join(newPath, ""))
 	_, err := exeCmd(buildCommandList)
 	if err != nil {
 		log.Fatal(err)
@@ -169,10 +171,10 @@ func runBuild() {
 			watch(exArgs, newRoot+"/src/"+packageName)
 		} else {
 			_, err := exeCmd(exArgs)
-            if err != nil {
-                log.Fatal(err)
-                return 
-            }
+			if err != nil {
+				log.Fatal(err)
+				return
+			}
 		}
 	} else {
 		log.Printf("Builded %s\n", funcName)
@@ -181,14 +183,17 @@ func runBuild() {
 
 func watch(args []string, rootPath string) {
 	done := make(chan error, 1)
+	doneWithoutErr := make(chan bool, 1)
 
 	cmd := getCmd(args)
 
 	go func() {
 		err := cmd.Run()
-        if err != nil {
-            done <- err
-        }
+		if err != nil {
+			done <- err
+		} else {
+			doneWithoutErr <- true
+		}
 	}()
 
 	restart := make(chan bool, 1)
@@ -207,12 +212,20 @@ func watch(args []string, rootPath string) {
 
 	select {
 	case <-restart:
-		if err := cmd.Process.Kill(); err != nil {
-			log.Fatal("failed to kill: ", err)
+		select {
+		case <-doneWithoutErr:
+			log.Println("process restarted")
+			runBuild()
+		default:
+			if err := cmd.Process.Kill(); err != nil {
+				log.Fatal("failed to kill: ", err)
+			}
+
+			log.Println("process restarted")
+			runBuild()
+
 		}
-        
-		log.Println("process restarted")
-        runBuild();
+
 	case err := <-done:
 		if err != nil {
 			log.Fatal("process done with error = %v", err)
